446fe7080ce2f2a651603cf311f19433c0a4a09e
sheremetvalfa <108634155+sheremetvalfa@users.noreply.github.com>
2022-07-10 14:42:52 -0400

    Add files via upload

---
+++ Phone.java
@@ -0,0 +1,13 @@
+public abstract class Phone  {
+    String name;
+    String model;
+
+    public Phone(String name, String model) {
+        this.name = name;
+        this.model = model;
+    }
+
+
+
+
+}
